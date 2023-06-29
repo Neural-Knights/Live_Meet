f930f536ad041c4e2d66ac799e943724522a1ec0
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-06-29 17:41:39 +0530

    Live Meet application made

---
+++ src/components/Header/HeaderComponents.jsx
@@ -0,0 +1,72 @@
+import { useMedia } from "react-use";
+import {
+  selectDominantSpeaker,
+  selectIsConnectedToRoom,
+  useHMSStore,
+} from "@100mslive/react-sdk";
+import { VolumeOneIcon } from "@100mslive/react-icons";
+import {
+  config as cssConfig,
+  Flex,
+  styled,
+  Text,
+  textEllipsis,
+  useTheme,
+} from "@100mslive/react-ui";
+import { useLogo } from "../AppData/useUISettings";
+import { isStreamingKit } from "../../common/utils";
+
+export const SpeakerTag = () => {
+  const dominantSpeaker = useHMSStore(selectDominantSpeaker);
+  return dominantSpeaker && dominantSpeaker.name ? (
+    <Flex
+      align="center"
+      justify="center"
+      css={{ flex: "1 1 0", color: "$textPrimary", "@md": { display: "none" } }}
+    >
+      <VolumeOneIcon />
+      <Text
+        variant="md"
+        css={{ ...textEllipsis(200), ml: "$2" }}
+        title={dominantSpeaker.name}
+      >
+        {dominantSpeaker.name}
+      </Text>
+    </Flex>
+  ) : (
+    <></>
+  );
+};
+
+const LogoImg = styled("img", {
+  maxHeight: "$16",
+  p: "$2",
+  w: "auto",
+  "@md": {
+    maxHeight: "$14",
+  },
+});
+
+export const Logo = () => {
+  const { themeType } = useTheme();
+  const logo = useLogo();
+  const isMobile = useMedia(cssConfig.media.md);
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  // Hide logo for now as there is not enough space
+  if (isConnected && isMobile && isStreamingKit()) {
+    return null;
+  }
+  return (
+    <LogoImg
+      src={
+        logo ||
+        (themeType === "dark"
+          ? require("../../images/logo-light.svg")
+          : require("../../images/logo-dark.svg"))
+      }
+      alt="Neural Knights Logo"
+      width={152}
+      height={60}
+    />
+  );
+};
---
+++ src/components/Preview/PreviewName.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Button, Input, styled } from "@100mslive/react-ui";
+import { isStreamingKit } from "../../common/utils";
+
+const PreviewName = ({ name, onChange, onJoin, enableJoin }) => {
+  const formSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <Form onSubmit={formSubmit}>
+      <Input
+        required
+        id="name"
+        css={{ w: "100%" }}
+        value={name}
+        onChange={e => onChange(e.target.value)}
+        className="input-preview"
+        placeholder="Enter your name here"
+        autoFocus
+        autoComplete="name"
+      />
+      <Button
+        type="submit"
+        className="button-preview"
+        disabled={!name || !enableJoin}
+        onClick={onJoin}
+      >
+        {isStreamingKit() ? "Join Studio" : "Join Room"}
+      </Button>
+    </Form>
+  );
+};
+
+const Form = styled("form", {
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  gap: "$4",
+  mt: "$10",
+  mb: "$10",
+  borderColor: "#18007B",
+});
+
+// const Input = styled("input", {
+//     borderStyle: "solid",
+//     borderWidth: 2,
+//     borderColor: "#18007B",
+//     borderRadius: 5,
+//   }
+// );
+
+export default PreviewName;
